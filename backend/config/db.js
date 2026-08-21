8765b4cd19ee7b1c92c90884be7fd51110d26647
Pravalika <vpravallika.gpm@gmail.com>
2026-08-21 13:30:20 +0530

    Use environment database connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,7 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "cogimp",
-  password: "Pravalika@45",
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
 });
 
 module.exports = pool;
